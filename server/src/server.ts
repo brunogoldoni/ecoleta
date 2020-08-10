384fea6da1461f6149c825de1a7dc4ab06594160
Bruno <bgoldoni@valid.com.br>
2020-08-10 19:24:52 -0300

    Application backend
    -routes and resources
    -http methods
    -params types

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,39 @@ import express from "express";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("Listagem de usuário");
+app.use(express.json());
 
-  response.json(["Bruno", "Carlos", "Chen", "Pingo"]);
+const users = ["Bruno", "Carlos", "Chen", "Pingo"];
+
+app.get("/users", (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search
+    ? users.filter((user) => user.includes(search))
+    : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get("/users/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user);
+});
+
+app.post("/users", (request, response) => {
+  const data = request.body;
+
+  console.log(data);
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
