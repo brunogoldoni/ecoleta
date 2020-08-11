0370ec12daba172758ee6542d9f6eb773fef4eb7
Bruno <bgoldoni@valid.com.br>
2020-08-11 19:43:41 -0300

    Application backend - Add cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import routes from "./routes";
+import cors from "cors";
 import path from "path";
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(routes);
 
