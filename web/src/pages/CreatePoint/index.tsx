bc978f34438890e552038e11a58c8e7e81476e0e
Bruno <bgoldoni@valid.com.br>
2020-08-15 10:56:25 -0300

    Front-end web - Searching API collection items

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 
 import { Map, TileLayer, Marker } from "react-leaflet";
 
+import api from "../../services/api";
+
 import logo from "../../assets/logo.svg";
 
 import "./styles.css";
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreatePoint = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    api.get("items").then((response) => {
+      setItems(response.data);
+    });
+  });
   return (
     <div id="page-create-point">
       <header>
@@ -87,30 +102,12 @@ const CreatePoint = () => {
           </legend>
 
           <ul className="items-grid">
-            <li className="selected">
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
-            <li>
-              <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de Cozinha</span>
-            </li>
+            {items.map((item) => (
+              <li className="selected" key={item.id}>
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </li>
+            ))}
           </ul>
         </fieldset>
 
