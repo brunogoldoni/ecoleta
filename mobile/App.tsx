7286ff5aadfbd0d41b5b9d8428fcc53c9a7766bd
Bruno <bgoldoni@valid.com.br>
2020-08-22 11:24:57 -0300

    Front-end mobile - create home page
    -add vector icons lib
    -add eslint typescript
    -implement layout screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
 
 import Home from './src/pages/Home';
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        translucent
+        barStyle="dark-content"
+        backgroundColor="transparent"
+      />
+      <Home />
+    </>
+  );
 }
 
 export default App;
