49886b6a2b4c9b458c6e4764748e26a158bbcfc9
Bruno <bgoldoni@valid.com.br>
2020-08-25 16:57:46 -0300

    Front-end mobile - maps
    -config and implement maps

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -8,106 +8,106 @@ import {useNavigation} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/Feather';
 
 const Home = () => {
-  const navigation = useNavigation();
+	const navigation = useNavigation();
 
-  function handleNavigateToPoints() {
-    navigation.navigate('Points');
-  }
+	function handleNavigateToPoints() {
+		navigation.navigate('Points');
+	}
 
-  return (
-    <ImageBackground
-      style={styles.container}
-      imageStyle={{width: 274, height: 368}}
-      source={require('../../assets/home-background.png')}>
-      <View style={styles.main}>
-        <Image source={require('../../assets/logo.png')} />
-        <Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
-        <Text style={styles.description}>
-          Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
-        </Text>
-      </View>
+	return (
+		<ImageBackground
+			style={styles.container}
+			imageStyle={{width: 274, height: 368}}
+			source={require('../../assets/home-background.png')}>
+			<View style={styles.main}>
+				<Image source={require('../../assets/logo.png')} />
+				<Text style={styles.title}>Seu marketplace de coleta de resíduos.</Text>
+				<Text style={styles.description}>
+					Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+				</Text>
+			</View>
 
-      <View style={styles.footer}>
-        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
-          <View style={styles.buttonIcon}>
-            <Text>
-              <Icon name="arrow-right" color="#FFF" size={24} />
-            </Text>
-          </View>
-          <Text style={styles.buttonText}>Entrar</Text>
-        </RectButton>
-      </View>
-    </ImageBackground>
-  );
+			<View style={styles.footer}>
+				<RectButton style={styles.button} onPress={handleNavigateToPoints}>
+					<View style={styles.buttonIcon}>
+						<Text>
+							<Icon name="arrow-right" color="#FFF" size={24} />
+						</Text>
+					</View>
+					<Text style={styles.buttonText}>Entrar</Text>
+				</RectButton>
+			</View>
+		</ImageBackground>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 32,
-  },
+	container: {
+		flex: 1,
+		padding: 32,
+	},
 
-  main: {
-    flex: 1,
-    justifyContent: 'center',
-  },
+	main: {
+		flex: 1,
+		justifyContent: 'center',
+	},
 
-  title: {
-    color: '#322153',
-    fontSize: 32,
-    fontFamily: 'Ubuntu-Bold',
-    maxWidth: 260,
-    marginTop: 64,
-  },
+	title: {
+		color: '#322153',
+		fontSize: 32,
+		fontFamily: 'Ubuntu-Bold',
+		maxWidth: 260,
+		marginTop: 64,
+	},
 
-  description: {
-    color: '#6C6C80',
-    fontSize: 16,
-    marginTop: 16,
-    fontFamily: 'Roboto-Regular',
-    maxWidth: 260,
-    lineHeight: 24,
-  },
+	description: {
+		color: '#6C6C80',
+		fontSize: 16,
+		marginTop: 16,
+		fontFamily: 'Roboto-Regular',
+		maxWidth: 260,
+		lineHeight: 24,
+	},
 
-  footer: {},
+	footer: {},
 
-  select: {},
+	select: {},
 
-  input: {
-    height: 60,
-    backgroundColor: '#FFF',
-    borderRadius: 10,
-    marginBottom: 8,
-    paddingHorizontal: 24,
-    fontSize: 16,
-  },
+	input: {
+		height: 60,
+		backgroundColor: '#FFF',
+		borderRadius: 10,
+		marginBottom: 8,
+		paddingHorizontal: 24,
+		fontSize: 16,
+	},
 
-  button: {
-    backgroundColor: '#34CB79',
-    height: 60,
-    flexDirection: 'row',
-    borderRadius: 10,
-    overflow: 'hidden',
-    alignItems: 'center',
-    marginTop: 8,
-  },
+	button: {
+		backgroundColor: '#34CB79',
+		height: 60,
+		flexDirection: 'row',
+		borderRadius: 10,
+		overflow: 'hidden',
+		alignItems: 'center',
+		marginTop: 8,
+	},
 
-  buttonIcon: {
-    height: 60,
-    width: 60,
-    backgroundColor: 'rgba(0, 0, 0, 0.1)',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
+	buttonIcon: {
+		height: 60,
+		width: 60,
+		backgroundColor: 'rgba(0, 0, 0, 0.1)',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 
-  buttonText: {
-    flex: 1,
-    justifyContent: 'center',
-    textAlign: 'center',
-    color: '#FFF',
-    fontFamily: 'Roboto-Medium',
-    fontSize: 16,
-  },
+	buttonText: {
+		flex: 1,
+		justifyContent: 'center',
+		textAlign: 'center',
+		color: '#FFF',
+		fontFamily: 'Roboto-Medium',
+		fontSize: 16,
+	},
 });
 
 export default Home;
